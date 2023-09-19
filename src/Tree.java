5895e329ae38d262e3339f5834bd718011c35781
Simon Noble <expectedb@outlook.com>
2023-09-19 14:07:27 -0400

    Added functions up to string()

--- src/Tree.java
+++ src/Tree.java
@@ -4,17 +4,15 @@ public class Tree<E> {
 
     private E root;
 
-    private ArrayList<Tree> subTrees;
+    private ArrayList<Tree<E>> subTrees;
 
-    public Tree(E root, ArrayList<Tree> subTrees ){
+    public Tree(E root, ArrayList<Tree<E>> subTrees ){
         this.root = root;
 
         this.subTrees = new ArrayList<>();
 
         if (subTrees != null){
-            for (Tree tree: subTrees){
-                this.subTrees.add(tree);
-            }
+            this.subTrees.addAll(subTrees);
         }
     }
 
@@ -44,7 +42,51 @@ public class Tree<E> {
         if (this.isEmpty()){
             return 0;
         }
+        int size = 1;
+
+        for (Tree<E> subtree: subTrees){
+            size += subtree.length();
+        }
+        return size;
     }
 
+    public int count(E item){
+        if (this.isEmpty()){
+            return 0;
+        }
+        int num = 0;
+        if (this.root == item){
+            num ++;
+        }
+
+        for (Tree<E> subtree: this.subTrees){
+            num += subtree.count(item);
+        }
+        return num;
+
+    }
+
+    public String string(){
+        return this.strIndented(0);
+    }
+
+
+    private String strIndented(int depth){
+        StringBuilder s = new StringBuilder();
+
+        if (this.isEmpty()){
+            return s.toString();
+        }
+
+        s.append(" ".repeat(Math.max(0, depth)));
+
+        s.append(this.root.toString());
+        s.append("\n");
+
+        for (Tree<E> subtree: this.subTrees){
+            s.append(subtree.strIndented(depth + 1));
+        }
+        return s.toString();
+    }
 
 }
