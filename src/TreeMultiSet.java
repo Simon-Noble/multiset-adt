e2edbe71a98172e60c72bffe489ff3f5324ec02c
Simon Noble <expectedb@outlook.com>
2023-09-20 00:22:55 -0400

    Initial commit, all functions implemented

---
+++ src/TreeMultiSet.java
@@ -0,0 +1,40 @@
+public class TreeMultiSet<E> extends MultiSet<E>{
+
+
+    private Tree<E> tree;
+
+    public TreeMultiSet(){
+        this.tree = new Tree<>();
+    }
+
+    @Override
+    public boolean add(E item) {
+        this.tree.insert(item);
+        return true;
+    }
+
+    @Override
+    public void remove(E item) {
+    this.tree.deleteItem(item);
+    }
+
+    @Override
+    public boolean contains(E item) {
+        return this.tree.contains(item);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.tree.isEmpty();
+    }
+
+    @Override
+    public int count(E item) {
+        return this.tree.count(item);
+    }
+
+    @Override
+    public int size() {
+        return this.tree.length();
+    }
+}
